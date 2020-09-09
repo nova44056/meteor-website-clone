0b2158468f00d80b6fc70758c19ef25e449841a3
ashura_44056 <sengouy0@gmail.com>
2020-09-09 12:45:19 +0700

    navbar homepage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="navbar">
+      <navbar/>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import navbar from './components/navbar'
+export default {
+  components: {
+    navbar
   }
+}
+</script>
+
+<style scoped>
+.navbar{
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="grid-container">
+      <div class="meteor-logo">
+        <img src="../assets/meteor-logo.svg">
+      </div>
+      <p class="div2">div2</p>
+      <p class="div3">div3</p>
+      <p class="div4">div4</p>
+      <p class="div5">div5</p>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.div2{
+    grid-area: 1 / 6 / 2 / 7; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.div3{
+    grid-area: 1 / 7 / 2 / 8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.div4{
+    grid-area: 1 / 8 / 2 / 9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.div5{
+     grid-area: 1 / 9 / 2 / 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.meteor-logo{
+    grid-area: 1 / 1 / 2 / 2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 5px;
+}
+
+.meteor-logo img{
+    height: 25px;
+}
+
+.grid-container {
+    /* align-items: center; */
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px; 
+    padding-top: 10px;
+    padding-bottom: 10pxs;
+}
+</style>
